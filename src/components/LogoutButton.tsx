2b18293fa164e926837ecd11e5aafa60eec08255
Caitlin <wolters.caitlin@gmail.com>
2024-05-29 11:33:24 -0700

    Add toast to logout

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,13 +1,21 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from '@radix-ui/themes';
+import { useStore } from '../store';
 
 export const LogoutButton: React.FC = () => {
   const { logout } = useAuth0();
+  const addToast = useStore((state) => state.addToast);
 
-  const handleLogout = () =>
+  const handleLogout = () => {
     logout({
       logoutParams: { returnTo: window.location.origin },
     });
+    addToast({
+      title: 'Success!',
+      message: 'You have been logged out.',
+      intent: 'success',
+    });
+  };
 
   return (
     <Button size="4" onClick={handleLogout} style={{ maxWidth: '150px' }}>
