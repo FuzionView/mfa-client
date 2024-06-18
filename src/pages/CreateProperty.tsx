c2aa99aa8478dd5ece811b962823375668581399
Caitlin <wolters.caitlin@gmail.com>
2024-06-18 11:57:36 -0700

    Add ability to cancel adding property

--- src/pages/CreateProperty.tsx
+++ src/pages/CreateProperty.tsx
@@ -1,6 +1,7 @@
 import { Flex, Button, Card } from '@radix-ui/themes';
 import { useCreatePropertyForm } from '../hooks/forms/useCreatePropertyForm';
 import { PropertyForm } from '@components';
+import { Link } from 'react-router-dom';
 
 export const CreateProperty: React.FC = () => {
   const { form, onSubmit, isSubmitPending } = useCreatePropertyForm();
@@ -12,9 +13,16 @@ export const CreateProperty: React.FC = () => {
           <Card>
             <PropertyForm form={form} />
           </Card>
-          <Button type="submit" style={{ alignSelf: 'flex-end' }} loading={isSubmitPending}>
-            Save
-          </Button>
+          <Flex style={{ alignSelf: 'flex-end' }} gap="1">
+            <Link to="/profile">
+              <Button loading={isSubmitPending} color="gray" variant="outline">
+                Cancel
+              </Button>
+            </Link>
+            <Button type="submit" loading={isSubmitPending}>
+              Save
+            </Button>
+          </Flex>
         </Flex>
       </form>
     </Flex>
